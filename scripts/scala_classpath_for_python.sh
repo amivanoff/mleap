d2612884bfeb6bf289e61e67cd489ae445fe8936
Aleksei Ivanov <amivanoff@gmail.com>
2025-01-01 19:47:55 -0500

    Disable Scala 2.12 in Python tests

--- scripts/scala_classpath_for_python.sh
+++ scripts/scala_classpath_for_python.sh
@@ -5,9 +5,9 @@ fi
 # Source this file to load SCALA_CLASS_PATH in the environment
 
 # Compile classes used in python tests
-${SBT} mleap-spark-extension/compile
-${SBT} mleap-spark/compile
+${SBT} "+ mleap-spark-extension/compile"
+${SBT} "+ mleap-spark/compile"
 
 # Export the complete dependencyClasspath into an env. variable
 export SCALA_CLASS_PATH=\
-"$( ${SBT} --error 'export mleap-spark-extension/runtime:fullClasspath' )"
+"$( ${SBT} --error '+ export mleap-spark-extension/runtime:fullClasspath' )"
